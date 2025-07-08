5598a414cb5d869770a8845cbe7fb8be00a1fede
Gabriel Romero Moscoso <grm-5@hotmail.com>
2025-07-07 23:22:33 -0600

    initial commit

---
+++ app/src/main/java/com/cardomomo/walkair/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.cardomomo.walkair
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ComponentActivity
+import com.cardomomo.walkair.ui.theme.WalkAirTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WalkAirTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun enableEdgeToEdge() {
+        TODO("Not yet implemented")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WalkAirTheme {
+        Greeting("Android")
+    }
+}
